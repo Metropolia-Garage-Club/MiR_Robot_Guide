294eb87ae4b77d3914378f750276267c9d274bf5
Nikk1bo <84156130+Nikk1bo@users.noreply.github.com>
2023-11-07 11:38:33 +0200

    Update MiR API test.py

--- MiR API test.py
+++ MiR API test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Niko Hutri 13.10.2023
+Niko Hutri 7.11.2023
 
 PasilaHUB MIR API test
 """
@@ -9,7 +9,7 @@ from tkinter import *
 import requests, json
 
 #get request
-ip = '192.168.0.61'
+ip = '192.168.0.51'
 host = 'http://' + ip + '/api/v2.0.0/'
 
 #Format Headers
@@ -17,15 +17,8 @@ headers = {}
 headers['Content.Type']= 'application/json'
 headers['Authorization'] = 'Basic ZGlzdHJpYnV0b3I6YjcyZDlkYzljMWYzNjg2MTBiMzBiZTJjZDc5OTAwOTAzYzk5MDYzNzA3NTY3M2NkMzliYjA4ZmJjYjc2YmIzYQ=='
 
-
+#you need one per mission
 def post_missions(host, headers):
-    mission_id = {"mission_id": "19989020-6757-11ee-8e81-000129922f30"}
+    mission_id = {"mission_id": "19989020-6757-11ee-8e81-000129922f30"} #Mission guid here
     post_mission = requests.post(host + 'mission_queue', json = mission_id, headers = headers)
     print(post_mission)
-window = Tk()
-window.title("MiR Rest Commands")
-window.geometry("100x150")
-
-btn1 = Button(window, text = "Go to place", command = post_missions(host, headers))
-btn1.place(x=10, y=10)
-window.mainloop()
