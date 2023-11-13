ce3df81188a44ab052f4ca53fd082598fcdf8e9f
J. Pitherin <jessica.pitherin@gmail.com>
2023-11-13 13:36:18 +0200

    Pushing changes from Master branch

--- backend/base.py
+++ backend/base.py
@@ -1,35 +1,9 @@
-from flask import Flask, render_template
-import time
-api = Flask(__name__)
+#https://stackoverflow.com/questions/40706854/splitting-a-flask-app-into-multiple-files
 
-wave_detected = False
-last_action_time = time.time()
+from flask import Flask #, Blueprint
+from wave_detection import wave_api
 
-#Check if more than 5 minutes have passed without any new actions
-def idle_check():
-    current_time = time.time()
-    return (current_time - last_action_time) > 300 #300 seconds = 5 minutes
-    
+main_api = Flask(__name__)
 
-@api.route('/detect_wave')
-
-def detect_wave():
-    response_body = {
-        "wave": True
-    }
-
-    return response_body
-
-
-@api.route('/')
-def index():
-    global wave_detected, last_action_time
-    if not wave_detected and idle_check():
-        return render_template('idle.js')
-    elif wave_detected:
-        return render_template('map1.js')
-    elif False:
-        return render_template('map2.js')
-    else:
-        return 'Unknown condition'
-    
+main_api.register_blueprint(wave_api)
+#main_app.register_blueprint(mir_api)
