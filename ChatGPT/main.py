5fa136d18590ed18ee05a1141e1187541e3bb45b
Yooru6 <yooru6@gmail.com>
2023-12-01 06:30:07 +0200

    Main.py updated

--- ChatGPT/main.py
+++ ChatGPT/main.py
@@ -2,11 +2,14 @@ import speech_recognition as sr
 import pyttsx3
 import voice as vc
 
-from flask import Flask, jsonify, request
-
 class Chatbot:
+    KEYWORD = "onni opas"
+    LOCATIONS = {"kirjasto": 0, "wc": 1, "auditorio": 2, "kahvila": 3, "hissi": 4, "ruokala": 5}
 
     def __init__(self):
+        self.initialize_components()
+
+    def initialize_components(self):
         self.r = sr.Recognizer()
         self.m = sr.Microphone()
         self.engine = pyttsx3.init()
@@ -17,53 +20,27 @@ class Chatbot:
     def _callback(self, recognizer, audio):
         try:
             words = recognizer.recognize_google(audio, language="fi-FI").lower()
-            print(">>>Asiakas: "+words+"\n")
-            if "onni opas" in words:
-                if "hei" in words:
-                    self.say("Hei olen Onni-Opas kuinka voin auttaa?")
-
-                #Paikat
-                elif "vie" in words or "mitkä" in words or "missä" in words:
-                    if "kirjasto" in words :
-                        print("->Lähdetään kirjastoon")
-                        vastaus= vc.route("0")
-                        self.say(vastaus)
-                    #WC    
-                    elif "wc" in words:
-                        print("->Lähdetään WC")
-                        vastaus= vc.route(1)
-                        self.say(vastaus)
-                    #auditorio
-                    elif "auditorio" in words:
-                        print("->Lähdetään Auditorio")
-                        vastaus= vc.route(2)
-                        self.say(vastaus)
-                    #Kahvila
-                    elif "kahvila" in words:
-                        print("->Lähdetään kahvilaan")
-                        vastaus= vc.route(3)
-                        self.say(vastaus)  
-                     #auditorio
-                    elif "hissi" in words:
-                        print("->Lähdetään hisseille")
-                        vastaus= vc.route(4)
-                        self.say(vastaus)         
-                     #auditorio
-                    elif "ruokala" in words:
-                        print("->Lähdetään ruokalaan")
-                        vastaus= vc.route(5)
-                        self.say(vastaus)                                   
-
-                else:
-                    self.say('Minulla ei ole lupaa vastata kysymykseen -> "' + words+'"')
-
+            print(f">>>Asiakas: {words}\n")
+            if self.KEYWORD in words:
+                self.handle_keyword(words)
             else:
-                print("Onni-Opas: Muista kutsua minua Onni-Oppaaksi jokaisen kysymyksen alussa tai muuten en osaa vastata kysymkseen...")
+                print("Onni-Opas: Muista kutsua minua Onni-Oppaaksi kysymyksen alussa.")
 
         except sr.UnknownValueError:
             print("Admin: OnniTulkki_11928.exe ei käynnisty. Esitä kysymys uudestaan.\n")
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            print(f"Could not request results from Google Speech Recognition service; {e}")
+
+    def handle_keyword(self, words):
+        if "hei" in words:
+            self.say("Hei olen Onni-Opas, kuinka voin auttaa?")
+        elif any(location in words for location in self.LOCATIONS):
+            location = next((loc for loc in self.LOCATIONS if loc in words), None)
+            print(f"->Paikka jonne sinut johdatan {location}")
+            vastaus = vc.route(self.LOCATIONS[location])
+            self.say(vastaus)
+        else:
+            self.say(f'Minulla ei ole lupaa vastata kysymykseen -> "{words}"')
 
     def listen(self):
         print("Admin: Muistin pyyhintä protokolla v0.116...")
@@ -75,7 +52,7 @@ class Chatbot:
         print("Admin: Järjestelmä päivitetty onnistuneesti! OnniOpas.exe käynnistetty. \n")
 
     def start(self):
-        self.listen();
+        self.listen()
         self.engine.startLoop()
         self.listening = True
         while self.listening:
@@ -88,9 +65,8 @@ class Chatbot:
         self.stop_listening()
 
     def say(self, msg):
-        print("Onni-opas: " + str(msg))
-        #self.engine.say(msg)
+        print(f"Onni-opas: {msg}")
+        # self.engine.say(msg)
 
 pop = Chatbot()
-
-pop.start();
+pop.start()
