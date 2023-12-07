1ff44d15407c5ff54565cbbbf5ed5ccadd7ce252
Yooru6 <yooru6@gmail.com>
2023-12-07 06:53:03 +0200

    Chatgpt lisätty ääniohjauksiin

--- ChatGPT/main.py
+++ ChatGPT/main.py
@@ -1,21 +1,27 @@
 import speech_recognition as sr
 import pyttsx3
 import voice as vc
+from gtts import gTTS
+import os
+import playsound
+import chatGPT
 
 class Chatbot:
     KEYWORD = "onni opas"
     LOCATIONS = {"kirjasto": 0, "wc": 1, "auditorio": 2, "kahvila": 3, "hissi": 4, "ruokala": 5}
     ROUTE_TRIGGERS = ["missä", "miten", "vie", "näytä", "johdata", "navigoi", "reitti"]
     GREETINGS=["hei", "moi", "moikka", "tere", "terve"]
+    GPT_KEYWORD=["kysymys", "kyssäri", "tiedätkö"]
 
     def __init__(self):
         self.initialize_components()
+        self.chatgpt_instance = chatGPT.Chatgpt()
 
     def initialize_components(self):
         self.r = sr.Recognizer()
         self.m = sr.Microphone()
         self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 100)
+        self.engine.setProperty('rate', 150)
         self.engine.connect('started-utterance', lambda name: self.stop())
         self.engine.connect('finished-utterance', lambda name, completed: self.listen())
 
@@ -39,12 +45,22 @@ class Chatbot:
         try:
             if any(keyword in words for keyword in self.ROUTE_TRIGGERS):
                 location = next((loc for loc in self.LOCATIONS if loc in words), None)
+                print("testi 1 "+location)
+
+                #Ensimmäinen osa chatgpt
                 if location:
                     print(f"-> Paikka jonne sinut johdatan: {location}")
                     vastaus = vc.route(self.LOCATIONS[location])
-                    self.say(vastaus)
+                    self.say("Opastan sinut paikkaan: "+str(location)+". Seuraa minua")
+            if any(keyword in words for keyword in self.GPT_KEYWORD):
+                question = next((loc for loc in self.GPT_KEYWORD if loc in words), None)
+                if question:
+                    print(f"-> Yritän vastata kysymykseen: {words}")
+                    response = self.chatgpt_instance.gpt_query(words) # kysely chat GPT:lle
+                    self.say(response)
+
             else:
-                self.say(f'Minulla ei ole lupaa vastata kysymykseen -> "{words}"')
+                self.say(f'Lähdetään matkaan')
         except Exception as e:
             print(f'Virhe käsittelyssä: {e}')
 
@@ -72,6 +88,15 @@ class Chatbot:
 
     def say(self, msg):
         print(f"Onni-opas: {msg}")
+
+        try:
+            speech = gTTS(msg, lang="fi")
+            speech_file = 'speech.mp3'
+            speech.save(speech_file)
+            playsound.playsound(speech_file)
+            
+        except Exception as e:
+            print(f'Error while handling speech: {e}')
         # self.engine.say(msg) #Tuon avamalla saa puheen takaisin
 
 pop = Chatbot()
